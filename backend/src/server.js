f24453bbc4135acb8b261be6404b03498f60fdcc
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-08-03 00:58:19 -0300

    fix: corrects express use of signup route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,15 @@
 import express,{json} from 'express';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import router from './routes/router.js';
-import connection from './database/postgres.js';
 
-
+dotenv.config();
 
 const app=express();
 
-app.use[cors(), json(), router];
+app.use([cors(), json(), router]);
 
 const PORT = process.env.PORT || 4001;
 
-try{
-    const test= await connection.query('SELECT * FROM users');
-    console.log(test);
-}catch(err){
-    console.log(err);
-}
 
-
-app.listen(PORT, console.log(`Server listening on ${PORT} port`));
+app.listen(PORT, ()=> console.log(`Server listening on ${PORT} port`));
