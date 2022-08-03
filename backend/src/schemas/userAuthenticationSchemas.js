1ca84f0b210797212b3f1e1db3308146692e4d38
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-08-03 01:00:57 -0300

    fix: fix validation schema to the  signup route

--- backend/src/schemas/userAuthenticationSchemas.js
+++ backend/src/schemas/userAuthenticationSchemas.js
@@ -1,10 +1,11 @@
 import joi from 'joi';
 
 const signupSchema = joi.object({
-    name: joi.string().required().min(3),
-    email: joi.string().email().required(),
-    password:joi.string().pattern(/(?=.*?[A-Z])/).pattern(/(?=.*?[a-z])/).pattern(/(?=.*?[0-9])/).pattern(/(?=.*?[#?!@$%^&*-])/).min(8).required(),
-    confirmPassword: joi.string().ref()
+    name: joi.string().required().min(3).max(150),
+    email: joi.string().email().max(255).required(),
+    password:joi.string().pattern(/(?=.*?[A-Z])/).pattern(/(?=.*?[a-z])/).pattern(/(?=.*?[0-9])/).pattern(/(?=.*?[#?!@$%^&*-])/).min(8).max(50).required(),
+    confirmPassword: joi.valid(joi.ref('password')).required(),
+    createdAt: joi.date().raw().optional(),
 });
 
 export  {signupSchema};
