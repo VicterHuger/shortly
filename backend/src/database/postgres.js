40863b8d9b5fecd0b66ddc8c6d77cf988b0b5c7e
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-08-07 15:10:13 -0300

    Prepare for deploy

--- backend/src/database/postgres.js
+++ backend/src/database/postgres.js
@@ -5,7 +5,10 @@ dotenv.config();
 const {Pool} = pg;
 
 const connection = new Pool({
-    connectionString: process.env.DATABASEURL,
+    connectionString: process.env.DATABASEURL,    
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default connection;
